b983edf484e0d3d788d81f8feeedc00b275aecd8
j <@>
2016-07-26 14:04:46 -0500

    merge various wdiff features to github.

--- wdiff-check.py
+++ wdiff-check.py
@@ -51,3 +51,73 @@ do_test('''
 +                        )
 ''',
 '\x1b[0m \n\x1b[7;32m                 if True:\n                     def func(u): return Z\n                 else:\n    \x1b[0m                 def func(u):\n\x1b[7;32m    \x1b[0m                     return (\n\x1b[7;32m    \x1b[0m                         Y\n\x1b[7;32m    \x1b[0m                     )\n')
+
+do_test('''
+-    def __contains__(self, bar):
+-        return bar in self.__items
++    def __contains__(self, foo):
++        assert isinstance(foo, int)
++        return foo in self.__items
+''',
+'\x1b[0m \n     def __contains__(self, \x1b[7;31mbar\x1b[7;32mfoo\x1b[0m):\n\x1b[7;32m         assert isinstance(foo, int)\n\x1b[0m         return \x1b[7;31mbar\x1b[7;32mfoo\x1b[0m in self.__items\n')
+
+do_test('''
+-        if not(
+-            (time.time() < go_time) # Check the time for that thing.
+-        and (g_got_things > need_thing_count) # Check those things.
+-        ):
++        can_do_checks = (
++            time.time() < go_time, # Check the time for that thing.
++            g_got_things > need_thing_count, # Check those things.
++        )
+''',
+'\x1b[0m \n         \x1b[7;31mif not\x1b[7;32mcan_do_checks = \x1b[0m(\n             \x1b[7;31m(\x1b[0mtime.time() < go_time\x1b[7;31m)\x1b[7;32m,\x1b[0m # Check the time for that thing.\n         \x1b[7;31mand (\x1b[7;32m    \x1b[0mg_got_things > need_thing_count\x1b[7;31m)\x1b[7;32m,\x1b[0m # Check those things.\n         )\x1b[7;31m:\x1b[0m \n')
+
+do_test('''
+-        if not(
+-            (that_thing_func() > THAT_IMPORTANT_THING) # Do the thing only when it's important.
+-        and (len(list_of_things)+1 < len(some_other_list_of_things))
+-        and (inside_thing() in list_of_insides) # Check whether it's in the list.
++        can_do_checks = (
++            (that_thing_func() > THAT_IMPORTANT_THING), # Do the thing only when it's important.
++            len(list_of_things)+1 < len(some_other_list_of_things),
++            inside_thing() in list_of_insides, # Check whether it's in the list.
+''', '''\x1b[0m \n         \x1b[7;31mif not\x1b[7;32mcan_do_checks = \x1b[0m(\n             (that_thing_func() > THAT_IMPORTANT_THING)\x1b[7;32m,\x1b[0m # Do the thing only when it's important.\n         \x1b[7;31mand (\x1b[7;32m    \x1b[0mlen(list_of_things)+1 < len(some_other_list_of_things)\x1b[7;31m)\n         and (\x1b[7;32m,\n             \x1b[0minside_thing() in list_of_insides\x1b[7;31m)\x1b[7;32m,\x1b[0m # Check whether it's in the list.\n''')
+
+do_test('''-if 1:
+-    one()
+-else:
+-    not()
+-    if 2:
+-        pass
++    if 1:
++        one()
+     else:
+-        do()
++        not()
++        if 2:
++            pass
++        else:
++            do()'''
+, '\x1b[7;32m    \x1b[0m if 1:\n\x1b[7;32m    \x1b[0m     one()\n\x1b[7;32m    \x1b[0m else:\n\x1b[7;32m    \x1b[0m     not()\n\x1b[7;32m    \x1b[0m     if 2:\n\x1b[7;32m    \x1b[0m         pass\n\x1b[7;32m    \x1b[0m     else:\n\x1b[7;32m    \x1b[0m         do()\x1b[7;31m \n')
+
+do_test(''' A foo
++B foo
+ C foo
+''', '\x1b[0m A foo\x1b[7;32m\n B foo\x1b[0m\n C foo\n')
+
+do_test('''
+-x   hithere b
+-y   weedog  z
+-sdf   sdfsdf  sdf
++x hithere    b
++y weedog     z
++sdf sdfsdf    sdf
+''', '\x1b[0m \n x \x1b[7;31m  \x1b[0mhithere\x1b[7;32m   \x1b[0m b\n y \x1b[7;31m  \x1b[0mweedog\x1b[7;32m   \x1b[0m  z\n sdf \x1b[7;31m  \x1b[0msdfsdf\x1b[7;32m  \x1b[0m  sdf\n')
+
+do_test('''
+-x   h b
+-y   w  z
++x h    b
++y w     z
+''', '\x1b[0m \n x \x1b[7;31m  \x1b[0mh\x1b[7;32m   \x1b[0m b\n y \x1b[7;31m  \x1b[0mw\x1b[7;32m   \x1b[0m  z\n')
--- wdiff.py
+++ wdiff.py
@@ -39,18 +39,13 @@ def canonicalize_doubles_blanks(diffs):
         else:
             i += 1
 
-def diff(old, new, inregs):
-    regs = list(inregs)
-    reg = regs.pop(0)
-    if regs:
-        regs[0] += reg
-
+def diff(old, new, reg):
     diffs = []
     oldsplit = [i for i in re.split('(['+reg+'])', old) if i]
     newsplit = [i for i in re.split('(['+reg+'])', new) if i]
-    #print oldsplit, newsplit
-    for oc in difflib.SequenceMatcher(None, oldsplit, newsplit).get_opcodes():
-        #print oc
+    #print oldsplit, '\n\n', newsplit
+    for oc in difflib.SequenceMatcher(None, oldsplit, newsplit, autojunk=False).get_opcodes():
+        #print "oc:  ", oc
         if (oc[0] == 'equal'):
             diffs.append( [ 'u', ''.join(oldsplit[oc[1]:oc[2]]) ] )
         elif oc[0] == 'replace':
@@ -63,30 +58,36 @@ def diff(old, new, inregs):
             diffs.append( [ 'a', ''.join(newsplit[oc[3]:oc[4]]) ] )
         else:
             raise
+        #print "dif: ", diffs
 
     while True:
         size = len(diffs)
+        #print '\n\n', diffs
 
         canonicalize_doubles_blanks(diffs)
+        #print '\n\n', diffs
 
         # weed out unreadable short u's
         i = 1
         while i < len(diffs)-1:
             if (
                    (diffs[i][0] == 'u')
-               and (('\n' not in diffs[i][1] and (len(diffs[i][1]) <= 2)) or re.match('\s+', diffs[i][1]))
+               and (('\n' not in diffs[i][1] and (len(diffs[i][1]) <= 2)) or re.match('\s+$', diffs[i][1]))
             ):
                 if diffs[i-1][0] != diffs[i+1][0]:
+                    #print 'a'*30, i, (diffs[i][0] == 'u'), diffs[i][1]
                     diffs[i-1][1] += diffs[i][1]
                     diffs[i+1][1] =  diffs[i][1] + diffs[i+1][1]
                     del diffs[i]
                     break
                 elif (i > 1) and (diffs[i-1][0] == diffs[i+1][0] and diffs[i-2][0] in ('a','d')) and (diffs[i-1][0] != diffs[i-2][0] ):
+                    #print 'b'*30, i, diffs[i]
                     diffs[i-2][1] += diffs[i][1]
                     diffs[i-1][1] += diffs[i][1]
                     del diffs[i]
                     break
                 elif (i + 2 < len(diffs)) and (diffs[i+1][0] != diffs[i+2][0] and diffs[i+2][0] in ('a','d')):
+                    #print 'c'*30, i, diffs[i]
                     diffs[i+1][1] = diffs[i][1] + diffs[i+1][1]
                     diffs[i+2][1] = diffs[i][1] + diffs[i+2][1]
                     del diffs[i]
@@ -99,18 +100,58 @@ def diff(old, new, inregs):
         if size == len(diffs):
             break
 
-    for i in range(1, len(diffs)-1):
-        # move spaces to beginning of line
-        if (
-            (diffs[i-1][0] == 'u') and (diffs[i][0] == 'a') and (diffs[i+1][0] == 'u') and
-            all(j == ' ' for j in diffs[i][1])
-        ):
-            rx = re.match('^([\s\S]*?)('+diffs[i][1]+'\s+)$', diffs[i-1][1])
-            if rx:
-                assert len(rx.groups()) == 2
-                diffs[i-1][1] = rx.groups()[0]
-                diffs[i+1][1] = rx.groups()[1]+diffs[i+1][1]
+    while True:
+        for i in range(1, len(diffs)-1):
+            # move spaces to beginning of line
+            if (
+                (diffs[i-1][0] == 'u') and (diffs[i][0] in ('a','d')) and (diffs[i+1][0] == 'u') and
+                all(j == ' ' for j in diffs[i][1])
+            ):
+                rx = re.match('^([\s\S]*?)('+diffs[i][1]+'\s+)$', diffs[i-1][1])
+                if rx:
+                    assert len(rx.groups()) == 2
+                    diffs[i-1][1] = rx.groups()[0]
+                    diffs[i+1][1] = rx.groups()[1]+diffs[i+1][1]
 
+            def ending_spaces_count(s):
+                for i, c in enumerate(reversed(s)):
+                    if c != ' ':
+                        return i
+                return len(s)
+
+
+            # consolidate added spaces to the beginning of an add/remove
+            esci = ending_spaces_count(diffs[i][1])
+            if (
+                esci and (diffs[i-1][0] == 'u') and (diffs[i][0] in ('a','d')) and (diffs[i+1][0] == 'u') and
+                esci == ending_spaces_count(diffs[i-1][1])
+            ):
+                diffs[i-1][1] =            diffs[i-1][1][:-esci]
+                diffs[i  ][1] = ' '*esci + diffs[i  ][1][:-esci]
+                diffs[i+1][1] = ' '*esci + diffs[i+1][1]
+
+            # move linebreak to beginning of an add/remove
+            lbspot = diffs[i][1].find('\n')
+            if (
+                (diffs[i-1][0] == 'u') and (diffs[i][0] in ('a','d')) and (diffs[i+1][0] == 'u') and
+                lbspot != -1 and
+                diffs[i][1][:lbspot] == diffs[i+1][1][:lbspot]
+            ):
+                diffs[i-1][1] += diffs[i+1][1][:lbspot]
+                diffs[i  ][1] =  diffs[i  ][1][lbspot:]+diffs[i+1][1][:lbspot]
+                diffs[i+1][1] =  diffs[i+1][1][lbspot:]
+
+            # Consolidate remove/add, with large chunk of end of remove matching beginning of the add. Perhaps a later version of difflib (or some tweak to our use of it) would fix this?
+            if (diffs[i][0] == 'd') and (diffs[i+1][0] == 'a'):
+                try:
+                    consolidate_len = max( j for j in range(min(len(diffs[i+1][1]), len(diffs[i][1]))) if diffs[i+1][1][:j] == diffs[i][1][-j:] )
+                except ValueError: pass
+                else:
+                    diffs[i+1][1] = diffs[i+1][1][consolidate_len:]
+                    diffs.insert(i+1, ['u', diffs[i][1][-consolidate_len:] ])
+                    diffs[i][1] = diffs[i][1][:-consolidate_len]
+                    break
+        else: break
 
 
     # output
@@ -119,11 +160,8 @@ def diff(old, new, inregs):
         assert len(sd) == 2,(sd, diffs)
         assert isinstance(sd[1], str),(sd, diffs)
         if sd[0] == 'u':
-            if regs:
-                diff(dels, adds, regs)
-            else:
-                colored_print(dels, 1)
-                colored_print(adds, 2)
+            colored_print(dels, 1)
+            colored_print(adds, 2)
             (adds, dels) = ('', '')
             colored_print(sd[1], 0)
         elif sd[0] == 'd':
@@ -133,17 +171,14 @@ def diff(old, new, inregs):
         else:
             raise
 
-    if regs:
-        diff(dels, adds, regs)
-    else:
-        colored_print(dels, 1)
-        colored_print(adds, 2)
+    colored_print(dels, 1)
+    colored_print(adds, 2)
 
 
 if __name__ == '__main__':
     def showdiffs(old, new):
         if old or new:
-            diff(old, new, ['|\n\r \t\"\';\$\%', ',<>\@_:\.\[\]()\'\\\/!{}'])
+            diff(old, new, '|\n\r \t\"\';\$\%,<>\@_:\.\[\]()\'\\\/!{}')
 
     (alines, dlines) = ('', '')
     for line in sys.stdin:
@@ -152,6 +187,9 @@ if __name__ == '__main__':
             alines += ' '+line[1:]
         elif line[0] == '-':
             dlines += ' '+line[1:]
+        elif line[0] == ' ':
+            alines += line
+            dlines += line
         else:
             showdiffs(dlines, alines)
             (alines, dlines) = ('', '')
