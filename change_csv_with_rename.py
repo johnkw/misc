8bd3de65f7c46ca2554315798da862df7c3f55a8
j <@>
2025-05-23 12:17:37 +0000

    allow for args and kwargs to callback

--- change_csv_with_rename.py
+++ change_csv_with_rename.py
@@ -1,9 +1,9 @@
 import csv, write_with_rename
 
-def perform(filepath, callback, dialect=csv.excel_tab):
+def perform(filepath, callback, dialect=csv.excel_tab, callback_args=[], callback_vargs={}):
     reader = csv.DictReader(open(filepath,'r'), dialect=dialect)
     with write_with_rename.WriteWithRename(filepath) as f:
         writer = csv.DictWriter(f, reader.fieldnames, dialect=dialect)
         writer.writeheader()
-        for row in callback(reader):
+        for row in callback(reader, *callback_args, **callback_vargs):
             writer.writerow(row)
