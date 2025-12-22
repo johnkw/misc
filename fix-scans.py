8883934958a240ae87d611e45979a306b74b2d29
j <@>
2025-12-22 16:22:09 +0000

    Tweak defaults.

--- fix-scans.py
+++ fix-scans.py
@@ -2,7 +2,7 @@
 
 import cmdargs, clog, datetime, functools, os, subprocess
 
-cmdargs.parse(('color',{'choices':['color','gray','bw']}),('--flips'))
+cmdargs.parse(('--color',{'choices':['color','gray','bw'],'default':'gray'}),('--flips'),('--quality',{'type':int,'default':15}),('--type',{'default':'avif'}))
 flips = [int(i) for i in cmdargs['flips'].split(',')] if cmdargs['flips'] else []
 os.chdir(os.path.expanduser('~/.tmp/scans'))
 scans = sorted((os.path.getmtime(i),i) for i in os.listdir() if i.startswith('Scan') and i.endswith('.png'))
@@ -10,7 +10,7 @@ scans = sorted((os.path.getmtime(i),i) for i in os.listdir() if i.startswith('Sc
 # This part is quite slow, so only do it once in case we want to change some other option after seeing the output.
 for i in scans:
     if not os.path.exists('trimmed-'+i[1]):
-        clog.debug('trimming '+i[1])
+        clog.info('trimming '+i[1])
         subprocess.check_call(['magick','-gravity','center',i[1],'-shave','20x20','-set','option:color','%[pixel:u.p{-1,1}]','-background','%[color]','+deskew','-despeckle','-fuzz','18%','-trim','+repage','-flatten','trimmed-'+i[1]])
 
 subprocess.check_call(
@@ -21,5 +21,5 @@ subprocess.check_call(
     )+
     ['-append','+repage','-strip','-flatten','-resize','2000x','-type','optimize']+
     (['-colorspace','gray'] if cmdargs['color'] in ('gray','bw') else [])+
-    (['-type','grayscale','-threshold','80%','-depth','1','-define','webp:lossless=true'] if cmdargs['color'] == 'bw' else ['-quality','50','-brightness-contrast','0x30'])+
-    [datetime.datetime.fromtimestamp(scans[-1][0]).strftime('%Y-%m-%d_%H_%M_%S')+('.webp' if cmdargs['color'] == 'bw' else '.jpg')])
+    (['-type','grayscale','-threshold','80%','-depth','1','-define','webp:lossless=true'] if cmdargs['color'] == 'bw' else ['-quality',str(cmdargs['quality']),'-brightness-contrast','0x30'])+
+    [datetime.datetime.fromtimestamp(scans[-1][0]).strftime('%Y-%m-%d_%H_%M_%S.'+cmdargs['type'])])
