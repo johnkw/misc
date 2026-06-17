2e5a7f89ebfb1a81051c008fb8a355b92785c141
j <@>
2026-06-17 01:29:03 +0000

    add compress option

--- write_with_rename.py
+++ write_with_rename.py
@@ -1,8 +1,8 @@
-import codecs, os, tempfile, time
+import io, lzma, os, tempfile, time
 
 class WriteWithRename(object):
-    __slots__ = ['filename', 'logging', 'binary', 'tf']
-    def __init__(self, filename, logging=True, binary=False):
+    __slots__ = ['filename', 'logging', 'binary', 'compress', 'tf']
+    def __init__(self, filename, logging=True, binary=False, compress=False):
         assert isinstance(filename, str)
         assert isinstance(binary,   bool)
         if logging is True:
@@ -13,13 +13,18 @@ class WriteWithRename(object):
         self.filename = filename
         self.logging  = logging
         self.binary   = binary
+        self.compress = compress
     def __enter__(self):
         if self.logging:
             self.logging('WriteWithRename writing '+('binary' if self.binary else 'text')+' '+self.filename)
         self.tf = tempfile.NamedTemporaryFile(dir=os.path.dirname(self.filename), delete=False)
-        if self.binary: return self.tf
-        return codecs.getwriter('utf-8')(self.tf.file)
+        if self.compress:
+            self.tf.file = lzma.LZMAFile(self.tf.file, 'a', format=lzma.FORMAT_XZ,preset=9|lzma.PRESET_EXTREME)
+        if not self.binary:
+            self.tf.file = io.TextIOWrapper(self.tf.file, write_through=True)
+        return self.tf.file
     def __exit__(self, _exc_type, _exc_value, _traceback):
+        self.tf.file.close()
         self.tf.close()
         if _exc_type == None:
             os.rename(self.tf.name, self.filename)
@@ -32,8 +37,8 @@ class WriteWithRename(object):
 
 class WriteCSV(WriteWithRename):
     __slots__ = ['dialect', 'fields']
-    def __init__(self, filename, fields, /, logging=True, binary=False, dialect='excel'):
-        super().__init__(filename, logging=logging, binary=binary)
+    def __init__(self, filename, fields, /, logging=True, binary=False, compress=False, dialect='excel'):
+        super().__init__(filename, logging=logging, binary=binary, compress=compress)
         self.dialect = dialect
         self.fields  = fields
     def __enter__(self):
